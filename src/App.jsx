283e9cdcce10b5a100a24abe2b180fab8bb72c87
KhushiGajul <gajulkhushi@gmail.com>
2026-04-22 11:16:41 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PartnersSignup from "./components/PartnersSignup";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <PartnersSignup />
+    </div>
+  )
+}
+
+export default App
